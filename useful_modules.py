76ecb9e12f7f825f66027f83498a4fe93ce3b580
nmynhier <43651565+nmynhier@users.noreply.github.com>
2018-10-09 10:22:58 -0700

    added files from session 4

---
+++ useful_modules.py
@@ -0,0 +1,9 @@
+import numpy as np					#the numpy library
+import matplotlib.pyplot as plt		#Matplotlib's pyplot
+
+import sys			#gives access to a C-like sys library
+import os			#gives access to operating systems
+
+
+print(sys.argv)		#prints any command line arguments, incl program name
+print(os.getcwd())	#print the current working dictionary
